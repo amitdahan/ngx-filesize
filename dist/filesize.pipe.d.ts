fa10ad68f29937b7daadf1c0956d3a3e51390b21
Amit Dahan <amit@payobin.com>
2018-03-06 14:54:07 +0200

    Removed dist folder from git

--- dist/filesize.pipe.d.ts
+++ dist/filesize.pipe.d.ts
@@ -1,5 +1,5 @@
 import { PipeTransform } from '@angular/core';
 export declare class FileSizePipe implements PipeTransform {
-    transform(value: number | number[], options: any): string | string[];
-    private static transformOne(value, options);
+    transform(value: number | number[], options?: any): string | string[];
+    private static transformOne(value, options?);
 }
