5a7ba9347695044d52cd755faf61553366570f43
Amit Dahan <amit@payobin.com>
2018-03-07 12:37:04 +0200

    Removed demo project

--- demo/src/main.ts
+++
@@ -1,6 +0,0 @@
-import 'zone.js';
-import 'core-js/es6/reflect';
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import {DemoModule} from './demo.module';
-
-platformBrowserDynamic().bootstrapModule(DemoModule);
