df11292d9249f0b695ce4ac4bf60fb74f18b5edb
Richard Lindner <richard.lindner@fluig.com>
2019-09-10 16:31:07 -0300

    Converted to angular 8 library

---
+++ projects/ngx-filesize/src/lib/filesize.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FileSizePipe } from './filesize.pipe';
+
+describe('FileSizePipe', () => {
+  it('create an instance', () => {
+    const pipe = new FileSizePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ projects/ngx-filesize/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of ngx-filesize
+ */
+
+export * from './lib/ngx-filesize.module';
+export * from './lib/filesize.pipe';
