746fd89d906a9729df5cad2c704c167a0b831805
CDDelta <me@cddelta.dev>
2021-04-29 13:13:56 +1000

    allow setting default file size options

--- projects/ngx-filesize/src/lib/filesize.pipe.ts
+++ projects/ngx-filesize/src/lib/filesize.pipe.ts
@@ -1,22 +1,29 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { Inject, InjectionToken, Optional, Pipe, PipeTransform } from '@angular/core';
 import * as filesize_ from 'filesize';
 
-type FileSizeOptions = Parameters<typeof filesize_>['1'];
+export type FileSizeOptions = Parameters<typeof filesize_>['1'];
+
+export const NGX_FILE_SIZE_DEFAULT_OPTIONS =
+    new InjectionToken<FileSizeOptions>('NGX_FILE_SIZE_DEFAULT_OPTIONS');
 
 @Pipe({
   name: 'filesize'
 })
 export class FileSizePipe implements PipeTransform {
+  constructor(@Optional() @Inject(NGX_FILE_SIZE_DEFAULT_OPTIONS) private readonly defaults?: FileSizeOptions) {}
+
   private static transformOne(value: number, options?: FileSizeOptions): string {
     const filesize = filesize_;
     return filesize(value, options);
   }
 
   transform(value: number | number[], options?: FileSizeOptions) {
+    const transformedOptions = this.defaults ? { ...this.defaults, ...options } : options;
+
     if (Array.isArray(value)) {
-      return value.map(val => FileSizePipe.transformOne(val, options));
+      return value.map(val => FileSizePipe.transformOne(val, transformedOptions));
     }
 
-    return FileSizePipe.transformOne(value, options);
+    return FileSizePipe.transformOne(value, transformedOptions);
   }
 }
