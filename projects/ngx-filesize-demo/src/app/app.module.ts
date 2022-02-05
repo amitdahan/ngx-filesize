9b7c266acababde4c494cff162949385d8e94ce7
Amit Dahan <amitdahan11@gmail.com>
2022-02-05 19:33:15 +0200

    Consume pipe module from demo app

--- projects/ngx-filesize-demo/src/app/app.module.ts
+++ projects/ngx-filesize-demo/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgxFilesizeModule } from 'ngx-filesize';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxFilesizeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
