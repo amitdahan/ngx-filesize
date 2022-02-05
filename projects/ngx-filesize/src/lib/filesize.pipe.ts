16fe04555cb2099f81d8766510ee8d8d1e47421c
Amit Dahan <amitdahan11@gmail.com>
2022-02-05 19:33:07 +0200

    Bring back original implementation

--- projects/ngx-filesize/src/lib/filesize.pipe.ts
+++ projects/ngx-filesize/src/lib/filesize.pipe.ts
@@ -1,13 +1,19 @@
 import { Pipe, PipeTransform } from '@angular/core';
-// import filesize_ from 'filesize';
+import filesize from 'filesize';
 
 @Pipe({
   name: 'filesize'
 })
 export class FilesizePipe implements PipeTransform {
-
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  private static transformOne(value: number, options?: any): string {
+    return filesize(value, options);
   }
 
+  transform(value: number | number[], options?: any) {
+    if (Array.isArray(value)) {
+      return value.map(val => FilesizePipe.transformOne(val, options));
+    }
+
+    return FilesizePipe.transformOne(value, options);
+  }
 }
