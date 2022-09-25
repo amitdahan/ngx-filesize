c7e2c22deeddc74e85a0b4e8abe12333190aa046
Amit Dahan <amitdahan11@gmail.com>
2022-09-25 17:47:07 +0300

    Migrate to angular 14

--- projects/ngx-filesize-demo/src/app/app.component.spec.ts
+++ projects/ngx-filesize-demo/src/app/app.component.spec.ts
@@ -1,11 +1,11 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { NgxFilesizeModule } from 'ngx-filesize';
 import * as filesize from 'filesize';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
@@ -13,19 +13,19 @@ describe('AppComponent', () => {
         NgxFilesizeModule
       ],
     }).compileComponents();
-  }));
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it('should use filesize pipe', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.filesize').textContent).toBe(
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.filesize')?.textContent).toBe(
       filesize(123456789)
     );
   });
--- projects/ngx-filesize/src/lib/ngx-filesize.module.ts
+++ projects/ngx-filesize/src/lib/ngx-filesize.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { FileSizePipe } from './filesize.pipe';
 
-
-
 @NgModule({
   declarations: [FileSizePipe],
   exports: [FileSizePipe]
