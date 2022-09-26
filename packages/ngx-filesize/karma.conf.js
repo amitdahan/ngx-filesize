8f55c4ec08ee7ae2ec29d30f2258d9edc106ccec
Amit Dahan <amitdahan11@gmail.com>
2022-09-26 18:58:41 +0300

    Split to 2 monorepo workspaces instead of single workspace with 2 "projects"

--- packages/ngx-filesize/karma.conf.js
+++ packages/ngx-filesize/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, '../../coverage/ngx-filesize-demo'),
+      dir: require('path').join(__dirname, './coverage/ngx-filesize'),
       subdir: '.',
       reporters: [
         { type: 'html' },
