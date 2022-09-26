50e7a9a3e771b57acec0d21bc65690b2e87d705c
Amit Dahan <amitdahan11@gmail.com>
2022-09-26 11:33:22 +0300

    Remove e2es

--- projects/ngx-filesize-demo/cypress/e2e/spec.cy.ts
+++
@@ -1,7 +0,0 @@
-describe('My First Test', () => {
-  it('Visits the initial project page', () => {
-    cy.visit('/')
-    cy.contains('app is running!')
-    cy.contains('.filesize', '117.74 MB')
-  })
-})
