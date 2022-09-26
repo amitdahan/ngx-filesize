91e74606609adace71e297b60ede1209cc231d0b
Amit Dahan <amitdahan11@gmail.com>
2022-09-26 11:15:56 +0300

    Fix cypress test

--- projects/ngx-filesize-demo/cypress/e2e/spec.cy.ts
+++ projects/ngx-filesize-demo/cypress/e2e/spec.cy.ts
@@ -2,5 +2,6 @@ describe('My First Test', () => {
   it('Visits the initial project page', () => {
     cy.visit('/')
     cy.contains('app is running!')
+    cy.contains('.filesize', '117.74 MB')
   })
 })
