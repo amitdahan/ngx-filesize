5c8e168756323bee33669447dd040ed250f2449a
Amit Dahan <amitdahan11@gmail.com>
2022-10-07 12:20:18 +0300

    Rename demo app and test using filesize@^6

--- packages/filesize-6-demo/karma.conf.js
+++ packages/filesize-6-demo/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ngx-filesize-demo'),
+      dir: require('path').join(__dirname, './coverage/filesize-6-demo'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- packages/filesize-6-demo/src/app/app.component.ts
+++ packages/filesize-6-demo/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ngx-filesize-demo';
+  title = 'filesize-6-demo';
 }
