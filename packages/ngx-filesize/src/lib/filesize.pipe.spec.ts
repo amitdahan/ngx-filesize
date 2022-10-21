7b323dc1186eb45b453e3e3bdd4b4770021fd747
Amit Dahan <amitdahan11@gmail.com>
2022-10-21 11:47:09 +0300

    fix tests

--- packages/ngx-filesize/src/lib/filesize.pipe.spec.ts
+++ packages/ngx-filesize/src/lib/filesize.pipe.spec.ts
@@ -4,10 +4,10 @@ describe('FileSizePipe', () => {
   const pipe = new FileSizePipe();
 
   it('transforms 1337', () => {
-    expect(pipe.transform(1337)).toBe('1.31 KB');
+    expect(pipe.transform(1337)).toBe('1.34 kB');
   });
 
   it('transforms 1024', () => {
-    expect(pipe.transform(1024)).toBe('1 KB');
+    expect(pipe.transform(1024)).toBe('1.02 kB');
   });
 });
